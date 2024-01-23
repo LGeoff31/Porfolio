f64699e6eb81817cdd376424326a04f1c536d56f
LGeoff31 <geoffrey31415@gmail.com>
2024-01-22 19:56:01 -0500

    added logo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,9 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      {/* <link rel="icon" href="../images/c++.png" />
-      </Head> */}
+      <Head>
+        <link rel="icon" href="../images/logo.png" />
+      </Head>
       {/* <Head /> */}
 
       <body>
