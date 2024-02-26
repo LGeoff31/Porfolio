548b25fb3be8077aab576c8f4ac97beea1b5a84b
LGeoff31 <geoffrey31415@gmail.com>
2024-02-25 19:22:31 -0500

    changed logo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="../images/logo_light.png" />
+        <link rel="icon" href="../images/logo_biggest.png" />
       </Head>
       {/* <Head /> */}
 
